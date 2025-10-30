239c9d8f9b76924c4ae507138f02bca1337fdaa3
majid-tech <majidmohammed044@gmail.com>
2025-10-30 00:06:25 +0000

    add react files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() { 
+  return (
+    <>
+      <div className="min-h-screen bg-slate-950">
+        <h1 className="text-white text-center p-8 text-4xl font-['Roboto']">Welcome to my Website</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
