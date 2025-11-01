04286e7a8739531c01cc586575041bc8536d1edd
majid-tech <majidmohammed044@gmail.com>
2025-11-01 11:08:46 +0000

    Add personal website live link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -72,7 +72,7 @@ export default function Projects() {
           </p>
           <div className="flex flex-col gap-3 justify-center mt-4 text-sm w-full">
             <a
-              href=""
+              href="https://personalwebsi.netlify.app/"
               target="_blank"
               className="px-4 py-2 bg-blue-600 rounded-lg hover:bg-blue-500 transition"
             >
